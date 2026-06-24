0e827f818c44f560ee4614b52a417d42d84e318d
dik-ab <abedaiki06@gmail.com>
2026-06-25 08:40:41 +0900

    Add cloudflare-site/astro.config.mjs

---
+++ cloudflare-site/astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  integrations: [mdx()],
+  output: 'static',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory'
+  },
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark'
+    }
+  }
+});
+
