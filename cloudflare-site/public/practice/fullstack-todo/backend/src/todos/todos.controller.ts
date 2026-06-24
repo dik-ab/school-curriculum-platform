7c691faf41c833280ef4cdbecda15611fcca130a
dik-ab <abedaiki06@gmail.com>
2026-06-25 08:40:42 +0900

    Add cloudflare-site/public/practice/fullstack-todo/backend/src/todos/todos.controller.ts

---
+++ cloudflare-site/public/practice/fullstack-todo/backend/src/todos/todos.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TodosService } from './todos.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+@Controller('todos')
+export class TodosController {
+  constructor(private readonly todosService: TodosService) {}
+
+  @Get()
+  findAll() {
+    return this.todosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.todosService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateTodoDto) {
+    return this.todosService.create(dto);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateTodoDto,
+  ) {
+    return this.todosService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(204)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.todosService.remove(id);
+  }
+}
